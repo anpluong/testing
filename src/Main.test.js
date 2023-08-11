71f7d4b455062b62a50c4f2449108e5e8160b6a2
An Luong <anpluong@gmail.com>
2023-08-11 00:57:22 -0500

    Cleaned up the unit testing

--- src/Main.test.js
+++ src/Main.test.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import Main from './Main';
 
 test('fetches data and sets it to state', () => {
   render(<Main />);
   let radio3 = screen.getByTestId("no_op");
+  let pTag = screen.getByTestId("p-tag");
   expect(radio3).toBeChecked();
+  expect(pTag).toHaveTextContent("no_op")
 
   let radio2 = screen.getByTestId("op_2");
   fireEvent.click(radio2)
   expect(radio2).toBeChecked();
+  expect(pTag).toHaveTextContent("op_2")
 
   let radio1 = screen.getByTestId("op_1");
-  expect(radio1).not.toBeChecked();
-
-  const pTag = screen.getByTestId("p-tag");
-
-  expect(pTag).toHaveTextContent("op_2")
+  fireEvent.click(radio1)
+  expect(radio1).toBeChecked();
+  expect(pTag).toHaveTextContent("op_1")
 
  
 });
