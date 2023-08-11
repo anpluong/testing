4e434365c42b77945efa585726a792db30255d84
An Luong <anpluong@gmail.com>
2023-08-11 00:26:22 -0500

    new component

--- src/Main.js
+++ src/Main.js
@@ -1,105 +1,155 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Main = () => {
-  const [radioOption, setRadioOption] = useState("");
-  const navigate = useNavigate();
-  const location = useLocation();
-  const path = location.pathname;
-  const [app1, setApp1] = useState(false);
-  const [app2, setApp2] = useState(false);
+function Main() {
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  // test1
-  useEffect(() => {
-    axios
-      .get(`https://localhost:8000/v1/services`)
-      .then((response) => {
-        setApp1(response.data.app1);
-        setApp2(response.data.app2);
-      });
-  });
-
-  // test2
-  const onOptionChange = (e) => {
-    const radioOptionValue = e.target.value;
-    setRadioOption(radioOptionValue);
-  };
-
-  // test3
-  const handleRedirect = async () => {
-    const config = {
-      method: "GET",
-      url: "http://localhost:8000/v1/login/authorization/a1?return_url=true",
-      header: {
-        sid: "AL",
-        swa: "12345-54342",
-      },
-    };
-
-    const response = await axios(config);
-    window.location.href = response.data.url;
-  };
-
-  // test 4
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    switch (radioOption) {
-      case "app1":
-        handleRedirect();
-        break;
-      case "app2":
-        navigate("/app2");
-        break;
-      case "app3":
-        navigate("/app3");
-        break;
-    }
+  const handleOptionChange = (event) => {
+    
+    setSelectedOption(event.target.value);
   };
 
   return (
-    <form>
-      <h1>Main App</h1>
-      {app1 && (
-        <section>
-          <div>
-            <input
-              value="app1"
-              name="app1"
-              type="radio"
-              id="app1"
-              onChange={onOptionChange}
-              checked={radioOption === "app1"}
-            />
-            <label htmlFor="app1">App1</label>
-          </div>
-        </section>
-      )}
-      {app2 && (
-        <section>
-          <div>
-            <input
-              value="app2"
-              name="app2"
-              type="radio"
-              id="app2"
-              onChange={onOptionChange}
-              checked={radioOption === "app2"}
-            />
-            <label htmlFor="app2">App2</label>
-          </div>
-        </section>
-      )}
-      <section>
-        <div>This is app3</div>
-      </section>
-      <section type='click' onClick={submitHandler}>
-        <button>next</button>
-      </section>
-    </form>
+    <div>
+      <form>
+        <label>
+          Option 1
+          <input
+            type="radio"
+            value="op_1"
+            data-testid="op_1"
+            checked={selectedOption === "op_1"}
+            onChange={handleOptionChange}
+          />
+        </label>
+        <label>
+          Option 2
+          <input
+            type="radio"
+            value="op_2"
+            data-testid="op_2"
+            checked={selectedOption === "op_2"}
+            onChange={handleOptionChange}
+          />
+        </label>
+        <label>
+          Option 3
+          <input
+            type="radio"
+            value="no_op"
+            data-testid="no-op"
+            checked={selectedOption === "no_op"}
+            onChange={handleOptionChange}
+          />
+        </label>
+      </form>
+      <p data-testid="p-tag">{selectedOption}</p>
+    </div>
   );
-};
+}
+
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { useNavigate, useLocation } from "react-router-dom";
+// import axios from "axios";
+
+// const Main = () => {
+//   const [radioOption, setRadioOption] = useState("");
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   const path = location.pathname;
+//   const [app1, setApp1] = useState(false);
+//   const [app2, setApp2] = useState(false);
+
+//   // test1
+//   useEffect(() => {
+//     axios
+//       .get(`https://localhost:8000/v1/services`)
+//       .then((response) => {
+//         setApp1(response.data.app1);
+//         setApp2(response.data.app2);
+//       });
+//   });
+
+//   // test2
+//   const onOptionChange = (e) => {
+//     const radioOptionValue = e.target.value;
+//     setRadioOption(radioOptionValue);
+//   };
+
+//   // test3
+//   const handleRedirect = async () => {
+//     const config = {
+//       method: "GET",
+//       url: "http://localhost:8000/v1/login/authorization/a1?return_url=true",
+//       header: {
+//         sid: "AL",
+//         swa: "12345-54342",
+//       },
+//     };
+
+//     const response = await axios(config);
+//     window.location.href = response.data.url;
+//   };
+
+//   // test 4
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+
+//     switch (radioOption) {
+//       case "app1":
+//         handleRedirect();
+//         break;
+//       case "app2":
+//         navigate("/app2");
+//         break;
+//       case "app3":
+//         navigate("/app3");
+//         break;
+//     }
+//   };
+
+//   return (
+//     <form>
+//       <h1>Main App</h1>
+//       {app1 && (
+//         <section>
+//           <div>
+//             <input
+//               value="app1"
+//               name="app1"
+//               type="radio"
+//               id="app1"
+//               onChange={onOptionChange}
+//               checked={radioOption === "app1"}
+//             />
+//             <label htmlFor="app1">App1</label>
+//           </div>
+//         </section>
+//       )}
+//       {app2 && (
+//         <section>
+//           <div>
+//             <input
+//               value="app2"
+//               name="app2"
+//               type="radio"
+//               id="app2"
+//               onChange={onOptionChange}
+//               checked={radioOption === "app2"}
+//             />
+//             <label htmlFor="app2">App2</label>
+//           </div>
+//         </section>
+//       )}
+//       <section>
+//         <div>This is app3</div>
+//       </section>
+//       <section type='click' onClick={submitHandler}>
+//         <button>next</button>
+//       </section>
+//     </form>
+//   );
+// };
 
 export default Main;
--- src/Main.test.js
+++ src/Main.test.js
@@ -1,100 +1,16 @@
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import axios from "axios";
-import Main from "./Main";
 
-const mockedUsedNavigate = jest.fn();
-const mockedUsedLocation = jest.fn();
+import Main from './Main';
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockedUsedNavigate,
-  useLocation: () => mockedUsedLocation,
-}));
+test('fetches data and sets it to state', () => {
+  render(<Main />);
 
-describe("YourComponent", () => {
-  it('should check the radio button when radioOption is "app1"', () => {
-    // // Arrange
-    // const onOptionChange = jest.fn(); // Mock the onOptionChange function
-    // const radioOption = 'app1';
-    // // Act
-    // const { getByLabelText } = render(
-    //   <>
-    //     <input
-    //       value="app1"
-    //       name="app1"
-    //       type="radio"
-    //       id="app1"
-    //       onChange={onOptionChange}
-    //       checked={radioOption === "app1"}
-    //     />
-    //     <label htmlFor="app1">app1</label>
-    //   </>
-    // );
-    // // Assert
-    // const radioInput = getByLabelText('app1');
-    // expect(radioInput.checked).toBe(true);
-  });
+  let radio1 = screen.getByTestId("op_1");
+  fireEvent.change(radio1)
 
-  // it('should call onOptionChange when radio button is changed', () => {
-  //   // Arrange
-  //   const onOptionChange = jest.fn(); // Mock the onOptionChange function
-  //   const radioOption = 'app2';
+  const pTag = screen.getByTestId("p-tag");
 
-  //   // Act
-  //   const { getByLabelText } = render(
-  //     <>
-  //       <input
-  //         value="app1"
-  //         name="app1"
-  //         type="radio"
-  //         id="app1"
-  //         onChange={onOptionChange}
-  //         checked={radioOption === "app2"}
-  //       />
-  //       <label htmlFor="app2">app2</label>
-  //     </>
-  //   );
-
-  //   const radioInput = getByLabelText('app');
-  //   fireEvent.click(radioInput);
-
-  //   // Assert
-  //   expect(onOptionChange).toHaveBeenCalled();
-  // });
+  expect(pTag).toHaveTextContent("op_1")
+ 
 });
-
-// const { getByText } = (
-//   <MemoryRouter>
-//     <Routes>
-//       <Route path="/start/:user" element={<Main />} />
-//     </Routes>
-//   </MemoryRouter>
-// );
-
-// getByText(/This is app3/);
-
-// given
-// const response = {
-//   app1: true,
-//   app2: true
-// };
-
-//  // when
-//  let result = await axios.get(
-//   "https://localhost:8000/v1/services"
-// );
-
-// axios.get = jest.fn(() => response);
-
-// // console.log(axios.get)
-// await waitFor(() => {
-
-//   expect((axios.get).toHaveBeenCalledWith(`https://localhost:8000/v1/services`));
-// })
-// expect(result).toEqual(response);
-
-
-/*  Reference
-https://github.com/brianspinos777/references/blob/main/javascript/react/mock-axios.md?fbclid=IwAR1MYSoWAqxb-tYnWMeITkq1lOyhGnon_wCiuHxBgN34wmSFM5bCbs-c460
-*/
